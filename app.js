ea373cae547835f7a61951f8c05d0f9eb3f9377a
Vikash Kumar <75295300+Vikashrock45@users.noreply.github.com>
2022-07-12 16:20:20 +0530

    Update App.js

--- app.js
+++ app.js
@@ -1,80 +1,73 @@
-const express = require("express");
+const express = require("express")
 const bodyParser = require("body-parser");
 const request = require("request");
-const https = require("https");
-
-const app = express();
-
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.get("/", function(req, res){
-	res.sendFile(__dirname+"/signup.html");
-});
-
-app.post("/", function(req, res){
-	const firstName = req.body.fname;
-	const lastName = req.body.lname;
-	const email = req.body.email;
-
-	const data = {
-		members: [
-		{
-			email_address: email,
-			status: "subscribed",
-			merge_fields: {
-				FNAME: firstName,
-				LNAME: lastName
-			}
+const mailchimp = require("@mailchimp/mailchimp_marketing")
+require('dotenv').config()
+const app = express()
+const https = require("https")
 
 
-		}]
-	};
+app.use(express.static("public"))
+app.use(express.urlencoded({extended: true}))
 
-	const jsonData = JSON.stringify(data);
-
-	const url = "https://us14.api.mailchimp.com/3.0/lists/a0e51f2031";
-
-	const options = {
-		method: "POST",
-		auth: "rock45:b091909a70e1e7e5744cb00073a891eb-us14"
-	}
-
-	const request = https.request(url, options, function(response){
-
-		if(response.statusCode === 200) {
-			res.sendFile(__dirname+"/success.html");
-		} else res.sendFile(__dirname+"/failure.html");
-		response.on("data", function(data){
-			console.log(JSON.parse(data));
-		});
-	});
-
-
-	request.write(jsonData);
-	request.end();
-
-
-	app.post("/failure", function(req, res){
-		res.redirect("/");
-	})
-
+//Setting up MailChimp
+mailchimp.setConfig({
+ apiKey: `${process.env.API_KEY}`,
+  server: "us14"
 })
 
-// Api Id - b091909a70e1e7e5744cb00073a891eb-us14
-// Aud Id - a0e51f2031
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(process.env.PORT || 3000, function(){
-	console.log("Server is running on port 3000");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html")
 });
+  
+
+app.post("/", function (req, res) {
+  const firstName = req.body.fName
+  const lastName = req.body.lName
+  const email = req.body.email
+
+  //console.log(firstName, lastName, email);
+
+  const data = {
+    members: [{
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+		}
+      }]
+  }
+
+  const jsonData = JSON.stringify(data)
+  
+  const url = "https://us14.api.mailchimp.com/3.0/lists/a0e51f2031"
+  
+  const options = {
+    method: "POST",
+    auth: `rock:${process.env.API_KEY}` 
+  }
+  
+  const request = https.request(url, options, function(response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html")
+    } else {
+      res.sendFile(__dirname + "/failure.html")
+    }
+
+    response.on("data", function (data){
+      console.log(JSON.parse(data))
+        });
+      });
+
+      //comment the request to test the de failure page
+      request.write(jsonData)
+      request.end()
+
+    })
+
+app.post("/failure", function (req, res) {
+  res.redirect("/")
+})
+
+app.listen(process.env.PORT || 3000, () => console.log("Server is running on port 3000"))
